1868bba6930b6690c73083ac09abd63bcf973dc7
wukunlun <wukunlun@bigai.ai>
2024-02-25 17:31:24 +0800

    First commit with simulated mode.

---
+++ Source/MegaMocapVRTest/MegaMocapVRTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MegaMocapVRTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEGAMOCAPVRTEST_API AMegaMocapVRTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
